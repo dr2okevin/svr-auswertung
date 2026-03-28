c60cafc91bef410b62dcd26a2de24c5113e4ebed
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2026-03-28 22:24:07 +0100

    Improve shots form

--- src/Form/ShotBatchRowType.php
+++ src/Form/ShotBatchRowType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,7 +15,7 @@ class ShotBatchRowType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('ShotIndex', IntegerType::class, [
+            ->add('ShotIndex', HiddenType::class, [
                 'label' => '#',
                 'disabled' => true,
             ])
